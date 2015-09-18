6f959f41e10624f52bf4d5aa9659fc595f6d0878
MrCrayfish <mrcrayfish@hotmail.com>
2015-09-19 00:29:00 +0930

    Added crafting recipe

--- src/main/java/com/mrcrayfish/key/MrCrayfishKeyMod.java
+++ src/main/java/com/mrcrayfish/key/MrCrayfishKeyMod.java
@@ -3,14 +3,16 @@ package com.mrcrayfish.key;
 import com.mrcrayfish.key.proxy.CommonProxy;
 
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
 public class MrCrayfishKeyMod
@@ -36,5 +38,7 @@ public class MrCrayfishKeyMod
 		proxy.init();
 		
 		MinecraftForge.EVENT_BUS.register(new KeyEvents());
+		
+		GameRegistry.addRecipe(new ItemStack(KeyItems.item_key), "NNI", 'I', Items.gold_ingot, 'N', Items.gold_nugget);
 	}
 }
